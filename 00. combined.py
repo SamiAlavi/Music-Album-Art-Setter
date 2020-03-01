a77eeec46155caae4ec1b4b31be88471b3ddb718
Sami Alavi <salavi.bese17seecs@seecs.edu.pk>
2020-03-01 15:04:12 +0500

    cleaned

--- 00. combined.py
+++ 00. combined.py
@@ -8,6 +8,18 @@ def setArt(song, art):
     mp3.picture=art
     mp3.write()
 
+def setArtRunner(path,files,audioforms):
+    for filename in files:
+        for formats in audioforms:
+            if checkformat(filename, formats):
+                try:
+                    setArt(path+filename,'downloads/'+filename+'.jpg')
+                except:
+                    with open('errors(setArt).txt','a+') as f:
+                        f.write(filename+'\n')
+                break
+
+
 def saveImage(iname,link):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     img_data = requests.get(link, headers=headers).content
@@ -23,45 +35,47 @@ def downloadImage(driver,url,query):
     #print(link)
     saveImage(query,link)
 
-def getAllArts(search_queries):
+def checkformat(query, formats):
+    return query.lower().endswith(formats)
+
+def getAllArts(search_queries, audioforms):
     options = webdriver.ChromeOptions()
     options.add_argument('headless') ##remove this to visualize the automation
     
     driver = webdriver.Chrome('chromedriver.exe',chrome_options=options) # Using Chrome to access
     url='https://www.ecosia.org/images?q='
     for query in search_queries:
-        try:
-            downloadImage(driver,url,query)
-        except:
-            with open('.errors(downloadImage).txt','a+') as f:
-                f.write(query+'\n')
+        for formats in audioforms:
+            if checkformat(query, formats):
+                print(query)
+                try:
+                    downloadImage(driver,url,query)
+                except:
+                    with open('.errors(downloadImage).txt','a+') as f:
+                        f.write(query+'\n')
+                break
     driver.quit()
 
-def setAlbum(path,music):
+def setAlbum(path,music,audioforms):
     with open("##COUNT.txt", "r") as f:
         count=int(f.read())
     
     for i in music:
-        count+=1
-        mp3=stagger.read_tag(path+i)
-        mp3.album=str(count)
-        mp3.write()
+        for formats in audioforms:
+            if checkformat(i, formats):
+                count+=1
+                mp3=stagger.read_tag(path+i)
+                mp3.album=str(count)
+                mp3.write()
+                break
         
     with open("##COUNT.txt", "w") as f:
         f.write(str(count))
 
 path='Music/'
 files = os.listdir(path)
+audioforms = ['.mp3'] #.mp3 supported
 
-getAllArts(files) #getAllArts called
-
-#setArt
-for i in files:        
-    try:
-        setArt(path+i,'downloads/'+i+'.jpg')
-    except:
-        with open('errors(setArt).txt','a+') as f:
-            f.write(i+'\n')
-            print(i)
-
-setAlbum(path,files) #setAlbum called
+getAllArts(files,audioforms) #getAllArts called
+setArtRunner(path,files,audioforms) #setArtRunner called
+setAlbum(path,files,audioforms) #setAlbum called
