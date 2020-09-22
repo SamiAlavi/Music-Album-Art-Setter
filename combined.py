6d8de3935bcadd9647553eb925b82c1ad2698cff
Sami Alavi <salavi.bese17seecs@seecs.edu.pk>
2020-09-22 14:50:37 +0500

    get and set lyrics added

--- combined.py
+++ combined.py
@@ -1,64 +1,60 @@
 from requests import get
 from stagger import read_tag
-from selenium import webdriver
 from os import listdir
+from eyed3 import load
 
+def checkformat(query, formats):
+    return query.lower().endswith(formats)
+
+#------------------SET ALBUM ART ------------------#
 def setArt(song, art):
-    mp3=read_tag(song)
-    mp3.picture=art
-    mp3.write()
+    try:
+        mp3=read_tag(song)
+        mp3.picture=art
+        mp3.write()
+    except:
+        with open('downloads/errors(setArt).txt','a+') as f:
+            f.write(f'{song} not found in downloads/images\n')
 
 def setArtRunner(path,files,audioforms):
     for filename in files:
         for formats in audioforms:
             if checkformat(filename, formats):
-                try:
-                    setArt(path+filename,'downloads/'+filename+'.jpg')
-                except:
-                    with open('errors(setArt).txt','a+') as f:
-                        f.write(filename+'not found in downloads\n')
+                setArt(path+filename,f'downloads/images/{filename}.jpg')
                 break
 
-
+#------------------GET ALBUM ART ------------------#
 def saveImage(iname,link):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     img_data = get(link, headers=headers).content
-    with open('downloads/'+iname+'.mp3.jpg', 'wb') as f:
+    with open(f'downloads/images/{iname}.jpg', 'wb') as f:
         f.write(img_data)
 
 def downloadImage(driver,url,query):
-    q=query.replace('&','').replace(' ','+')+' album&size=large'
-    cssselect = 'a.image-result'
-    driver.get(url+q)
+    try:
+        q=query[:-4].replace('&','').replace(' ','+')+' album&size=large'
+        driver.get(url+q)
 
-    link = driver.find_element_by_css_selector(cssselect).get_attribute('href')
-    #print(link)
-    saveImage(query,link)
+        link = driver.find_element_by_css_selector('a.image-result').get_attribute('href')
+        #print(link)
+        saveImage(query,link)
+    except:
+        with open('downloads/errors(downloadImage).txt','a+') as f:
+            f.write(query+' not found\n')
 
-def checkformat(query, formats):
-    return query.lower().endswith(formats)
-
-def getAllArts(search_queries, audioforms):
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless') ##remove this to visualize the automation
-    
-    driver = webdriver.Chrome('chromedriver.exe',chrome_options=options) # Using Chrome to access
+def getAllArts(driver,search_queries, audioforms):
     url='https://www.ecosia.org/images?q='
     for i in range(len(search_queries)):
         query = search_queries[i]
-        if query+'.jpg' in listdir('downloads/'):
+        if query+'.jpg' in listdir('downloads/images/'):
             continue
         for formats in audioforms:
             if checkformat(query, formats):
-                print(query)
-                try:
-                    downloadImage(driver,url,query[:-4])
-                except:
-                    with open('.errors(downloadImage).txt','a+') as f:
-                        f.write(query+' not found\n')
+                print(f'{i+1}) {query}')
+                downloadImage(driver,url,query)
                 break
-    driver.quit()
-
+            
+#------------------ ALBUM NUMBER ------------------#            
 def setAlbum(path,music,audioforms):
     with open("#COUNT.txt", "r") as f:
         count=int(f.read())
@@ -75,3 +71,75 @@ def setAlbum(path,music,audioforms):
     with open("#COUNT.txt", "w") as f:
         f.write(str(count))
 
+#----------------- SET LYRICS ------------------#
+def setLyrics(song, fname):
+    try:
+        with open(fname,'r') as f:
+            lyrics = f.read()
+        mp3=load(song)
+        tagg = mp3.tag
+        tagg.lyrics.set(lyrics)
+        tagg.save(song)
+    except:
+        with open('downloads/errors(setLyrics).txt','a+') as f:
+            f.write(f'{song} not found in downloads/lyrics\n')
+
+def setLyricsRunner(path,files,audioforms):
+    for filename in files:
+        for formats in audioforms:
+            if checkformat(filename, formats):
+                setLyrics(path+filename,f'downloads/lyrics/{filename}.txt')
+                break
+    
+#------------------GET LYRICS ------------------#
+def writelyrics(song,lyrics):
+    if lyrics is not None:
+        lyrics = lyrics.replace('’',"'")
+        with open(f'downloads/lyrics/{song}.txt','w', encoding='utf-8') as f:
+            f.write(lyrics)
+
+def downloadLyrics(driver,url,song):
+    flag = True
+    geniuss = 'genius'
+    azlyricss = 'azlyrics.com/lyrics'
+    
+    q = song[:-4].replace(' ','+').replace('&','%26')+' lyrics'
+    driver.get(url+q)
+
+    elems = driver.find_elements_by_css_selector('a.result-title')
+    for elem in elems:
+        link = elem.get_attribute("href")
+        if geniuss in link:
+            driver.get(link)
+            try:
+                elem = driver.find_element_by_css_selector('div.jgQsqn')
+            except:
+                xpath = '/html/body/routable-page/ng-outlet/song-page/div/div/div[2]/div[1]/div/defer-compile[1]/lyrics/div/div/section/p'
+                elem = driver.find_element_by_xpath(xpath)
+            writelyrics(song,elem.text)
+            flag = False
+            break
+        elif azlyricss in link:
+            driver.get(link)
+            elem = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[5]')
+            writelyrics(song,elem.text)
+            flag = False
+            break
+    if flag:
+        with open('downloads/errors(getLyrics).txt', 'a', encoding='utf-8') as f:
+            f.write(song+'\n')
+
+def getAllLyrics(driver,files,audioforms):
+    azlyricss = 'azlyrics.com/lyrics'
+    geniuss = 'genius'
+    url='https://www.ecosia.org/search?q='
+
+    for i in range(len(files)):
+        query = files[i]
+        if query+'.txt' in listdir('downloads/lyrics/'):
+            continue
+        for formats in audioforms:
+            if checkformat(query, formats):
+                print(f'{i+1}) {query}')
+                downloadLyrics(driver,url,query)
+                break
--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 import os
-from combined import getAllArts, setArtRunner, setAlbum
+from selenium import webdriver
+from combined import getAllArts, setArtRunner, getAllLyrics, setLyricsRunner, setAlbum
+
+def setDriver():
+    #options to make selenium faster
+    prefs = {'profile.default_content_setting_values': {'images': 2, 
+            'plugins': 2, 'popups': 2, 'geolocation': 2, 
+            'notifications': 2, 'auto_select_certificate': 2, 'fullscreen': 2, 
+            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2, 
+            'media_stream_mic': 2, 'media_stream_camera': 2, 'protocol_handlers': 2, 
+            'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2, 
+            'push_messaging': 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop': 2, 
+            'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement': 2, 
+            'durable_storage': 2}}
+    options = webdriver.ChromeOptions()    
+    options.add_experimental_option("prefs", prefs)
+    #options.add_argument('headless') ##remove this to visualize the automation
+    driver = webdriver.Chrome('chromedriver.exe',options=options) # Using Chrome to access
+    return driver
 
 path='Music/'
 files = os.listdir(path)    
 audioforms = ['.mp3'] #.mp3 supported
+options = ['n','y']
 
 if __name__ == '__main__':
-    print('Total files:',len(files))
-    getAllArts(files,audioforms) #getAllArts called
-    setArtRunner(path,files,audioforms) #setArtRunner called
-    setAlbum(path,files,audioforms) #setAlbum called
+    # options
+    flag1 = input('Find album arts? (n/Y) ').lower()
+    while flag1 not in options:
+        flag1 = input('Find album arts? (n/Y) ').lower()
+    
+    flag2 = input('Find music lyrics? (N/y) ').lower()
+    while flag2 not in options:
+        flag2 = input('Find music lyrics? (N/y) ').lower()
+
+    flag3 = input('Rename album names? (N/y) ').lower()
+    while flag3 not in options:
+        flag3 = input('Rename album names? (N/y) ').lower()
+        
+    driver = setDriver()
+    print('Total files:',len(files)-1)
+    
+    if flag1=='y':
+        getAllArts(driver,files,audioforms) #getAllArts called
+        setArtRunner(path,files,audioforms) #setArtRunner called
+        pass
+    
+    if flag2=='y':
+        getAllLyrics(driver,files,audioforms) #getAllLyrics called
+        setLyricsRunner(path,files,audioforms) #setLyricsRunner called
+    driver.quit()
+
+    if flag3=='y':
+        setAlbum(path,files,audioforms) #setAlbum called
+        pass
