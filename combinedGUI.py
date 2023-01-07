6b933bdb4f4f2e66827b963e2295d8cab25338d9
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-01-08 02:50:07 +0500

    SA: cleanup

--- combinedGUI.py
+++ combinedGUI.py
@@ -1,18 +1,5 @@
 from dialog import Dialog
-import requests
-from bs4 import BeautifulSoup
-from stagger import read_tag
-from os import listdir
-from eyed3 import load
-import os
-from subprocess import call
 from tkinter import messagebox
-import src.helper.helper as helper
-from src.helper.helper_path import unhide_directory
-#from time import sleep
-########################################################################
-
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 def album_arts_runner(files_names):
     from src.album_art import start_album_arts_runner
