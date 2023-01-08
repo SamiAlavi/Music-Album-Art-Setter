2e50c9538800786df7710c0622e58a396b2335cb
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-01-08 20:14:33 +0500

    SA: refactor build; add multiple platforms support;

--- build.py
+++ build.py
@@ -1,11 +1,26 @@
+import sys
 import PyInstaller.__main__
+from src.helper.helper_gui import APP_ICON, PLATFORM_WINDOWS, PLATFORM_LINUX, PLATFORM_OSX, EXTENSION_XBM, EXTENSION_ICO
+
+def get_icon_path(icon_name):
+    extension = EXTENSION_ICO
+    if sys.platform == PLATFORM_LINUX or sys.platform == PLATFORM_OSX:
+        extension = EXTENSION_XBM
+    elif sys.platform == PLATFORM_WINDOWS:
+        extension = EXTENSION_ICO
+    return f'{icon_name}.{extension}'
+
+icon_path = get_icon_path(APP_ICON)
+distribution_name = 'GUI'
+script_name = 'GUI.py'
 
 PyInstaller.__main__.run([
     '--add-data',
-    'music.ico;.',
+    f'{icon_path};.',
     '--windowed',
     '--clean',
-    '--icon=music.ico',
-    '--name=GUI',
-    'GUI.py',
+    '--noconfirm',
+    f'--icon={icon_path}',
+    f'--name={distribution_name}',
+    f'{script_name}',
 ])
